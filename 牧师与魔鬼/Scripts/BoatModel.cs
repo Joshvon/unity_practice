60dc4db72011be38a20f5f65cd62e45654a63bf8
hjfeng <1211498189@qq.com>
2022-10-24 23:56:40 +0800

    main

--- 牧师与魔鬼/Scripts/BoatModel.cs
+++ 牧师与魔鬼/Scripts/BoatModel.cs
@@ -4,108 +4,108 @@ using UnityEngine;
 using conInterface;
 
 public class BoatModel
+{
+	GameObject boat;                                          
+	Vector3[] start_empty_pos;                                    
+	Vector3[] end_empty_pos;                                      
+	Move move;                                                    
+	ClickGUI click;
+	int boat_sign = 1;                                                     
+	RoleModel[] roles = new RoleModel[2];                                  
+
+	public BoatModel()
 	{
-		GameObject boat;                                          
-		Vector3[] start_empty_pos;                                    
-		Vector3[] end_empty_pos;                                      
-		Move move;                                                    
-		ClickGUI click;
-		int boat_sign = 1;                                                     
-		RoleModel[] roles = new RoleModel[2];                                  
+		boat = Object.Instantiate(Resources.Load("Prefabs/Boat", typeof(GameObject)), new Vector3(25, -2.5F, 0), Quaternion.identity) as GameObject;
 
-		public BoatModel()
+		boat.name = "boat";
+		move = boat.AddComponent(typeof(Move)) as Move;
+		click = boat.AddComponent(typeof(ClickGUI)) as ClickGUI;
+		click.SetBoat(this);
+		start_empty_pos = new Vector3[] { new Vector3(18, 4, 0), new Vector3(32,4 , 0) };
+		end_empty_pos = new Vector3[] { new Vector3(-32, 4, 0), new Vector3(-18,3 , 0) };
+	}
+
+	public bool IsEmpty()
+	{
+		for (int i = 0; i < roles.Length; i++)
 		{
-			boat = Object.Instantiate(Resources.Load("Prefabs/Boat", typeof(GameObject)), new Vector3(25, -2.5F, 0), Quaternion.identity) as GameObject;
-
-			boat.name = "boat";
-			move = boat.AddComponent(typeof(Move)) as Move;
-			click = boat.AddComponent(typeof(ClickGUI)) as ClickGUI;
-			click.SetBoat(this);
-			start_empty_pos = new Vector3[] { new Vector3(18, 4, 0), new Vector3(32,4 , 0) };
-			end_empty_pos = new Vector3[] { new Vector3(-32, 4, 0), new Vector3(-18,3 , 0) };
+			if (roles[i] != null)
+				return false;
 		}
+		return true;
+	}
 
-		public bool IsEmpty()
+	public void BoatMove()
+	{
+		if (boat_sign == -1)
 		{
-			for (int i = 0; i < roles.Length; i++)
-			{
-				if (roles[i] != null)
-					return false;
-			}
-			return true;
+			move.MovePosition(new Vector3(25, -2.5F, 0));
+			boat_sign = 1;
 		}
-
-		public void BoatMove()
+		else
 		{
-			if (boat_sign == -1)
-			{
-				move.MovePosition(new Vector3(25, -2.5F, 0));
-				boat_sign = 1;
-			}
-			else
-			{
-				move.MovePosition(new Vector3(-25, -2.5F, 0));
-				boat_sign = -1;
-			}
-		}
-
-		public int GetBoatSign(){ return boat_sign;}
-
-		public RoleModel DeleteRoleByName(string role_name)
-		{
-			for (int i = 0; i < roles.Length; i++)
-			{
-				if (roles[i] != null && roles[i].GetName() == role_name)
-				{
-					RoleModel role = roles[i];
-					roles[i] = null;
-					return role;
-				}
-			}
-			return null;
-		}
-
-		public int GetEmptyNumber()
-		{
-			for (int i = 0; i < roles.Length; i++)
-			{
-				if (roles[i] == null)
-				{
-					return i;
-				}
-			}
-			return -1;
-		}
-
-		public Vector3 GetEmptyPosition()
-		{
-			Vector3 pos;
-			if (boat_sign == -1)
-				pos = end_empty_pos[GetEmptyNumber()];
-			else
-				pos = start_empty_pos[GetEmptyNumber()];
-			return pos;
-		}
-
-		public void AddRole(RoleModel role)
-		{
-			roles[GetEmptyNumber()] = role;
-		}
-
-		public GameObject GetBoat(){ return boat; }
-
-		public int[] GetRoleNumber()
-		{
-			int[] count = { 0, 0 };
-			for (int i = 0; i < roles.Length; i++)
-			{
-				if (roles[i] == null)
-					continue;
-				if (roles[i].GetSign() == 0)
-					count[0]++;
-				else
-					count[1]++;
-			}
-			return count;
+			move.MovePosition(new Vector3(-25, -2.5F, 0));
+			boat_sign = -1;
 		}
 	}
+
+	public int GetBoatSign(){ return boat_sign;}
+
+	public RoleModel DeleteRoleByName(string role_name)
+	{
+		for (int i = 0; i < roles.Length; i++)
+		{
+			if (roles[i] != null && roles[i].GetName() == role_name)
+			{
+				RoleModel role = roles[i];
+				roles[i] = null;
+				return role;
+			}
+		}
+		return null;
+	}
+
+	public int GetEmptyNumber()
+	{
+		for (int i = 0; i < roles.Length; i++)
+		{
+			if (roles[i] == null)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public Vector3 GetEmptyPosition()
+	{
+		Vector3 pos;
+		if (boat_sign == -1)
+			pos = end_empty_pos[GetEmptyNumber()];
+		else
+			pos = start_empty_pos[GetEmptyNumber()];
+		return pos;
+	}
+
+	public void AddRole(RoleModel role)
+	{
+		roles[GetEmptyNumber()] = role;
+	}
+
+	public GameObject GetBoat(){ return boat; }
+
+	public int[] GetRoleNumber()
+	{
+		int[] count = { 0, 0 };
+		for (int i = 0; i < roles.Length; i++)
+		{
+			if (roles[i] == null)
+				continue;
+			if (roles[i].GetSign() == 0)
+				count[0]++;
+			else
+				count[1]++;
+		}
+		return count;
+	}
+}
--- 牧师与魔鬼/Scripts/LandModel.cs
+++ 牧师与魔鬼/Scripts/LandModel.cs
@@ -3,79 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LandModel
+{
+	GameObject land;                                
+	Vector3[] positions;                            
+	int land_sign;                                  
+	RoleModel[] roles = new RoleModel[6];       
+		
+	public LandModel(string land_mark)
 	{
-		GameObject land;                                
-		Vector3[] positions;                            
-		int land_sign;                                  
-		RoleModel[] roles = new RoleModel[6];       
-            
-		public LandModel(string land_mark)
+		positions = new Vector3[] {new Vector3(46F,14.73F,-4), new Vector3(55,14.73F,-4), new Vector3(64F,14.73F,-4),
+			new Vector3(73F,14.73F,-4), new Vector3(82F,14.73F,-4), new Vector3(91F,14.73F,-4)};
+		if (land_mark == "start")
 		{
-			positions = new Vector3[] {new Vector3(46F,14.73F,-4), new Vector3(55,14.73F,-4), new Vector3(64F,14.73F,-4),
-				new Vector3(73F,14.73F,-4), new Vector3(82F,14.73F,-4), new Vector3(91F,14.73F,-4)};
-			if (land_mark == "start")
-			{
-				land = Object.Instantiate(Resources.Load("Prefabs/Land", typeof(GameObject)), new Vector3(70, 1, 0), Quaternion.identity) as GameObject;
-				land_sign = 1;
-			}
-			else if(land_mark == "end")
-			{
-				land = Object.Instantiate(Resources.Load("Prefabs/Land", typeof(GameObject)), new Vector3(-70, 1, 0), Quaternion.identity) as GameObject;
-				land_sign = -1;
-			}
+			land = Object.Instantiate(Resources.Load("Prefabs/Land", typeof(GameObject)), new Vector3(70, 1, 0), Quaternion.identity) as GameObject;
+			land_sign = 1;
 		}
-
-		public int GetEmptyNumber()                      
+		else if(land_mark == "end")
 		{
-			for (int i = 0; i < roles.Length; i++)
-			{
-				if (roles[i] == null)
-					return i;
-			}
-			return -1;
+			land = Object.Instantiate(Resources.Load("Prefabs/Land", typeof(GameObject)), new Vector3(-70, 1, 0), Quaternion.identity) as GameObject;
+			land_sign = -1;
 		}
-
-		public int GetLandSign() { return land_sign; }
-
-		public Vector3 GetEmptyPosition()               
-		{
-			Vector3 pos = positions[GetEmptyNumber()];
-			pos.x = land_sign * pos.x;                  
-			return pos;
-		}
-
-		public void AddRole(RoleModel role)             
-		{
-			roles[GetEmptyNumber()] = role;
-		}
-
-		public RoleModel DeleteRoleByName(string role_name)      
-		{ 
-			for (int i = 0; i < roles.Length; i++)
-			{
-				if (roles[i] != null && roles[i].GetName() == role_name)
-				{
-					RoleModel role = roles[i];
-					roles[i] = null;
-					return role;
-				}
-			}
-			return null;
-		}
-
-		public int[] GetRoleNum()
-		{
-			int[] count = { 0, 0 };                    
-			for (int i = 0; i < roles.Length; i++)
-			{
-				if (roles[i] != null)
-				{
-					if (roles[i].GetSign() == 0)
-						count[0]++;
-					else
-						count[1]++;
-				}
-			}
-			return count;
-		}	
 	}
+
+	public int GetEmptyNumber()                      
+	{
+		for (int i = 0; i < roles.Length; i++)
+		{
+			if (roles[i] == null)
+				return i;
+		}
+		return -1;
+	}
+
+	public int GetLandSign() { return land_sign; }
+
+	public Vector3 GetEmptyPosition()               
+	{
+		Vector3 pos = positions[GetEmptyNumber()];
+		pos.x = land_sign * pos.x;                  
+		return pos;
+	}
+
+	public void AddRole(RoleModel role)             
+	{
+		roles[GetEmptyNumber()] = role;
+	}
+
+	public RoleModel DeleteRoleByName(string role_name)      
+	{ 
+		for (int i = 0; i < roles.Length; i++)
+		{
+			if (roles[i] != null && roles[i].GetName() == role_name)
+			{
+				RoleModel role = roles[i];
+				roles[i] = null;
+				return role;
+			}
+		}
+		return null;
+	}
+
+	public int[] GetRoleNum()
+	{
+		int[] count = { 0, 0 };                    
+		for (int i = 0; i < roles.Length; i++)
+		{
+			if (roles[i] != null)
+			{
+				if (roles[i].GetSign() == 0)
+					count[0]++;
+				else
+					count[1]++;
+			}
+		}
+		return count;
+	}	
+}
